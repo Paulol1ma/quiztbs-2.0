cdde523cb4cdc461663bd8314cccca7f79b502c6
Paulo Matheus <108484313+Paulol1ma@users.noreply.github.com>
2022-08-23 14:28:36 -0300

    quiz tbs

---
+++ quiz 2.0/questions.js
@@ -0,0 +1,76 @@
+let questions = [
+    {
+        question: ' (1)  Em qual sistema eu peço uma caneta para meu setor',
+        options: [
+            'Ocomon',
+            'Purchasing',
+            'Sick Bay',
+            'Justificativa'
+        ],
+        answer: 1
+    },
+    {
+        question: '(2) Sistema justificativa serve para?',
+        options: [
+            'Justificar meu chamado que eu abri',
+            'Justifico que preciso de um teclado',
+            'Relatar minha falta no mês anterior',
+            'Justificar meu pedido de compras',
+            
+        ],
+        answer: 2
+    },
+    {
+        question: '(3) gyywwqyqwy',
+        options: [
+            '0',
+            '1',
+            '2',
+           
+        ],
+        answer: 2
+    },
+    {
+        question: '(4) Sistema Ocomon eu abro chamado para trocar o quadro do meu setor',
+        options: [
+            'Verdade',
+            'Falso',
+        ],
+        answer: 1
+    },
+    {
+        question: '(5) Sistema Ocomon eu abro chamado para trocar o quadro do meu setor',
+        options: [
+            'Verdade',
+            'Falso',
+        ],
+        answer: 1
+    },
+    {
+        question: '(6) Sistema Ocomon eu abro chamado para trocar o quadro do meu setor',
+        options: [
+            'Verdade',
+            'Falso',
+        ],
+        answer: 1
+    },
+    {
+        question: '(7) Sistema Ocomon eu abro chamado para trocar o quadro do meu setor',
+        options: [
+            'Verdade',
+            'Falso',
+        ],
+        answer: 1
+    },
+    {
+        question: '(8) Sistema Ocomon eu abro chamado para trocar o quadro do meu setor',
+        options: [
+            'Verdade',
+            'Falso',
+        ],
+        answer: 1
+    },
+
+      
+   
+];
